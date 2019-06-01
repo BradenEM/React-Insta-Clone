075009001dd4039b3d58f024d2ec6cbcc4ac7615
Braden Mason <bradenemason@gmail.com>
2019-05-31 20:00:15 -0500

    added PostsPage and moved rendering items to there

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -1,49 +1,17 @@
 import React from "react";
-import data from "./dummy-data";
-import SearchBar from "./components/SearchBar/SearchBar";
 import "./App.css";
-import PostContainer from "./components/PostContainer/PostContainer";
+import PostsPage from "./components/PostsPage/PostsPage";
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      posts: [],
-      searchInput: "",
-      filteredPosts: []
-    };
+    this.state = {};
   }
-
-  componentDidMount() {
-    this.setState({ posts: data });
-  }
-
-  commenceSearch = e => {
-    e.preventDefault();
-    let posts = this.state.posts.filter(post => {
-      if (post.username.includes(e.target.value)) {
-        return post;
-      }
-    });
-    this.setState({ filteredPosts: posts });
-  };
 
   render() {
-    console.log(this.state.posts);
     return (
       <div>
-        <div>
-          <SearchBar commenceSearch={this.commenceSearch} />
-        </div>
-        <div>
-          <PostContainer
-            posts={
-              this.state.filteredPosts.length > 0
-                ? this.state.filteredPosts
-                : this.state.posts
-            }
-          />
-        </div>
+        <PostsPage />
       </div>
     );
   }
